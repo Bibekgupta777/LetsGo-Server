7265371f300ebe3a8e4c649f2cd2f966e0f41234
Bibekgupta777 <vivek.gupta.22265@gmai.com>
2025-01-19 09:24:37 +0545

    Created route controller and model, also seat model only

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("Database Connected");
+        console.log("Database connection stablished successfully");
     } catch (error) {
         console.log("Error connecting to the database:", error);
     }
--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
 const userRoutes = require('./routes/userRoutes');
+const busRoutes = require("./routes/busRoutes");
+const routeRoutes = require("./routes/routeRoutes");
 
 connectDB();
 const PORT = process.env.PORT ? process.env.PORT : 5001;
@@ -12,6 +14,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api/user", userRoutes);
+app.use("/api/bus", busRoutes);
+app.use("/api/route", routeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
