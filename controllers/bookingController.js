1bde317a06435d0ada65f18a1118029f4c758f1e
Bibekgupta777 <vivek.gupta.22265@gmai.com>
2025-02-20 10:22:18 +0545

    controller code of booking

---
+++ controllers/bookingController.js
@@ -0,0 +1,4 @@
+const Booking = require("../models/Booking");
+const Schedule = require("../models/Schedule");
+const Seat = require("../models/Seat");
+const Notification = require("../models/Notification");
