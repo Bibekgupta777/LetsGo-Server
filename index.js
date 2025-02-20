a396e2c547164d6b7c3dce87cb745a81659d8d65
Bibekgupta777 <vivek.gupta.22265@gmai.com>
2025-02-20 10:41:36 +0545

    routing of schedule and booking

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const userRoutes = require('./routes/userRoutes');
 const busRoutes = require("./routes/busRoutes");
 const seatRoutes = require("./routes/seatRoutes");
 const routeRoutes = require("./routes/routeRoutes");
+const scheduleRoutes = require("./routes/scheduleRoutes");
+const bookingRoutes = require("./routes/bookingRoutes");
 
 connectDB();
 const PORT = process.env.PORT ? process.env.PORT : 5001;
@@ -18,6 +20,8 @@ app.use("/api/user", userRoutes);
 app.use("/api/bus", busRoutes);
 app.use("/api/seats", seatRoutes);
 app.use("/api/route", routeRoutes);
+app.use("/api/schedule", scheduleRoutes);
+app.use("/api/booking", bookingRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
